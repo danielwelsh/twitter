8d3ed7337b94dc47ac819f4d1cdfe86b46d7a538
Govind Rai <raigovind93@gmail.com>
2017-04-01 13:18:20 -0700

    Fine tune migrations/models and add seed data

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,3 +1,15 @@
 class Tweet < ActiveRecord::Base
-  # Remember to create a migration!
+  belongs_to :user
+  has_many :tweet_tags
+  has_many :tags, through: :tweet_tags
+  after_create :parse_tags
+
+  def parse_tags
+    pattern = /#[a-z]{3,}/
+    hash_tags = self.tweet.scan(pattern)
+    hash_tags.each do |hash_tag|
+      tag = Tag.create(tag: hash_tag)
+      TweetTag.create(tweet: self, tag: tag)
+    end
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,24 @@
-User.create(handle: '@tom', email: 'tom@tom.com', password: 'tomtom', first_name: 'tom', last_name: 'tom')
+require 'faker'
 
-Tweet.create()
+tom = User.new(handle: '@tom', email: 'tom@tom.com', password: 'tomtom', first_name: 'tom', last_name: 'tom')
+tom.save!
+tom1 = User.new(handle: '@tom1', email: 'tom1@tom1.com', password: 'tom1tom1', first_name: 'tom1', last_name: 'tom1')
+tom1.save!
+tom2 = User.new(handle: '@tom2', email: 'tom2@tom2.com', password: 'tom2tom2', first_name: 'tom2', last_name: 'tom2')
+tom2.save!
+tom3 = User.new(handle: '@tom3', email: 'tom3@tom3.com', password: 'tom3tom3', first_name: 'tom3', last_name: 'tom3')
+tom3.save!
+
+User.all.each do |user|
+  10.times do
+    tweet = Tweet.new(tweet: Faker::Hacker.say_something_smart, user: user)
+    tweet.save!
+    tweet2 = Tweet.new(tweet: "hashtags galore #this #that #blessed #yomomma #up", user: user)
+  end
+end
+
+Following.new(user:tom, following_id:tom2.id)
+Following.new(user:tom, following_id:tom3.id)
+Following.new(user:tom3, following_id:tom.id)
+Following.new(user:tom2, following_id:tom.id)
+
