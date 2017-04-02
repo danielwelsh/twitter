3bbe973e570f3eb00a3e4a4d76bacc57ba24abbc
Govind Rai <raigovind93@gmail.com>
2017-04-01 20:05:26 -0700

    implement error handling for registration

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,13 +1,15 @@
 get '/' do
   if current_user
+    p "-" * 25
     @tweets = get_tweets_feed(current_user)
+    p "-" * 25
     erb :'show'
   else
+    p "*" * 25
+    @user = User.new()
+    p "*" * 25
     if params[:error_type]
       @login_error = 'Incorrect handle/password.'
-    else
-      @signup_errors = 'blah'
-      #error logic for sign ups
     end
     erb :index
   end
@@ -25,8 +27,29 @@ post '/login' do
 end
 
 post '/signup' do
+  @signup_errors = {}
   @user = User.find_by(handle: params[:user][:handle])
-  # if
+
+  if @user
+    @user = User.new(params[:user])
+    @signup_errors = "A twitter account with that handle already exists"
+    erb :'index'
+  else
+    @user = User.new(params[:user])
+    if params[:user][:password] == "" and params["password-confirm"] == ""
+      @signup_errors[:password] = ["Password fields cannot be blank"]
+    end
+    if params[:user][:password] != params["password-confirm"]
+      @signup_errors[:password] = ["Passwords don't match"]
+    end
+    if @user.save
+      login_user
+      redirect '/'
+    else
+      @signup_errors = @signup_errors.merge(@user.errors.messages)
+      erb :'index'
+    end
+  end
 end
 
 post '/logout' do
