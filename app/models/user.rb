cc414d5005259c72ba6b594f2fbf392e95b7117b
Daniel Welsh <danielwelsh@borealproductions.com>
2017-04-02 01:43:44 -0700

    Add almost all associations to users.

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,11 +2,11 @@ require 'bcrypt'
 class User < ActiveRecord::Base
   include BCrypt
   has_many :tweets # works
-  has_many :followings
-  has_many :followers, through: :followings, foreign_key: :following_id
-  has_many :liked_tweets
-  has_many :replied_tweets
-  has_many :retweets, through: :tweets, foreign_key: :retweet_id
+  has_many :followings # works
+  has_many :followers # works and returns followers objects
+  has_many :liked_tweets #works and returned liked tweet objects
+  has_many :replied_tweets #works and returns replied tweet objects
+  has_many :retweets, through: :tweets, foreign_key: :retweet_id #
 
   validates :first_name, :last_name, :handle, :email, :password_hash, presence:true
   validates :handle, :email, uniqueness: true
@@ -34,4 +34,21 @@ class User < ActiveRecord::Base
     self.last_name = self.last_name.capitalize
   end
 
+  def get_nested_objects(self_association_method, pluck_field_sym, class_name)
+    nested_objects_ids = self_association_method.pluck(pluck_field_sym)
+    nested_objects_ids_string = nested_objects_ids.reduce('(') { |final_string, id| final_string + id.to_s + ','}.chop + ')'
+    class_name.where("id in #{nested_objects_ids_string}")
+  end
+
+  def get_followers
+    get_nested_objects(self.followers, :follower_id, User)
+  end
+
+  def get_liked_tweets
+    get_nested_objects(self.liked_tweets, :tweet_id, Tweet)
+  end
+
+  def get_replied_tweets
+    get_nested_objects(self.replied_tweets, :tweet_id, Tweet)
+  end
 end
