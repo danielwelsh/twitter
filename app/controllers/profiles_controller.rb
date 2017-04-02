0572a6d5fc0a3cce370baf80f5edbf74d06e853b
Daniel Welsh <danielwelsh@borealproductions.com>
2017-04-01 19:06:57 -0700

    Add like, reply, retweet, and create forms

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -7,6 +7,38 @@
 
 # update '/like' do
 
+
+#CREATE
+post '/tweets/create' do
+  Tweet.create(params[:tweet])
+  redirect '/'
+end
+
+
+#REPLY
+post '/tweets/:tweet_id/reply' do
+  @tweet = Tweet.find(params[:tweet])
+  redirect '/'
+end
+
+
+#LIKE
+post '/tweets/:tweet_id/like' do
+  @tweet = Tweet.find(:tweet_id)
+  likes = @tweets.likes
+  @tweet.update(likes: (likes + 1))
+  redirect '/'
+end
+
+#RETWEET
+post '/tweets/:tweet_id/retweet' do
+  @tweet = Tweet.find(:tweet_id)
+
+  #Recreates that same tweet for a person.
+  new_tweet = Tweet.create(user_id: current_user.id, tweet: @tweet.tweet, replied_to_id: @tweet)
+end
+
+
 put '/like' do
   @user = User.find(params[:user_id])
   @tweet = Tweet.find(params[:tweet_id])
@@ -19,3 +51,5 @@ end
 put '/retweet' do
   @user = User.find(params[:user_id])
 end
+
+
