c2d365a5d25cf7d906202e25c8d06dddb9f4405a
Daniel Welsh & Govind Rai <pair+danielwelsh.govindrai@devbootcamp.com>
2017-04-01 17:58:39 -0700

    implement sessions_controller WIP

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,3 +1,29 @@
 get '/' do
-  erb :index
+  if current_user
+    @tweets = current_user.get_tweets_feed
+    erb :'show'
+  else
+    if params[:error_type]
+      @login_error = 'Incorrect handle/password.'
+    else
+      @signup_errors = 'blah'
+      #error logic for sign ups
+    end
+    erb :index
+  end
+end
+
+post '/login' do
+    @user = User.find_by(handle: params[:handle])
+  p @user
+  if @user && @user.authenticate(params[:password])
+    login_user
+    redirect '/'
+  else
+    redirect '/?error_type=login'
+  end
+end
+
+post '/signup' do
+
 end
