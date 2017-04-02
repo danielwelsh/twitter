54d54a4926e1177350042717ed44397670e0137e
Daniel Welsh <danielwelsh@borealproductions.com>
2017-04-02 01:43:44 -0700

    Add almost all associations to users.

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,6 +7,7 @@ Tag.all.destroy_all
 TweetTag.all.destroy_all
 LikedTweet.all.destroy_all
 RepliedTweet.all.destroy_all
+Follower.all.destroy_all
 
 tom = User.new(handle: 'tom', email: 'tom@tom.com', password: 'tomtom', first_name: 'tom', last_name: 'tom')
 tom.save!
@@ -34,12 +35,15 @@ Following.new(user:tom, following_id: tom1.id).save
 # Following.new(user:tom3, following_id:tom.id).save
 Following.new(user:tom1, following_id: tom.id).save
 
-LikedTweet.new(user: tom, tweet: Tweet.find(11)).save
-LikedTweet.new(user: tom, tweet: Tweet.find(12)).save
-LikedTweet.new(user: tom, tweet: Tweet.find(13)).save
-LikedTweet.new(user: tom, tweet: Tweet.find(14)).save
-LikedTweet.new(user: tom, tweet: Tweet.find(15)).save
-LikedTweet.new(user: tom, tweet: Tweet.find(16)).save
+Follower.new(user:tom, follower_id: tom1.id).save
+Follower.new(user:tom1, follower_id: tom.id).save
+
+LikedTweet.new(user: tom, tweet: Tweet.find(21)).save
+LikedTweet.new(user: tom, tweet: Tweet.find(22)).save
+LikedTweet.new(user: tom, tweet: Tweet.find(23)).save
+LikedTweet.new(user: tom, tweet: Tweet.find(24)).save
+LikedTweet.new(user: tom, tweet: Tweet.find(25)).save
+LikedTweet.new(user: tom, tweet: Tweet.find(26)).save
 
 
 LikedTweet.new(user: tom1, tweet: Tweet.find(1)).save
@@ -51,8 +55,8 @@ LikedTweet.new(user: tom1, tweet: Tweet.find(6)).save
 
 
 #LEARN: CHECK TO SEE IF THERE IS A WAY TO PLACE OBJECTS IN HERE INSTEAD
-RepliedTweet.new(user: tom, tweet: Tweet.create(tweet: Faker::Hacker.say_something_smart, user: tom), replied_to_tweet_id: 12).save
-RepliedTweet.new(user: tom, tweet: Tweet.create(tweet: Faker::Hacker.say_something_smart, user: tom), replied_to_tweet_id: 13).save
+RepliedTweet.new(user: tom, tweet: Tweet.create(tweet: Faker::Hacker.say_something_smart, user: tom), replied_to_tweet_id: 22).save
+RepliedTweet.new(user: tom, tweet: Tweet.create(tweet: Faker::Hacker.say_something_smart, user: tom), replied_to_tweet_id: 23).save
 
 
 RepliedTweet.new(user: tom1, tweet: Tweet.create(tweet: Faker::Hacker.say_something_smart, user: tom1), replied_to_tweet_id: 8).save
