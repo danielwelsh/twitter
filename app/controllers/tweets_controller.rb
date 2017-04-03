df928969590e316497af52ed1632656ba89acf8f
Daniel Welsh <danielwelsh@borealproductions.com>
2017-04-03 00:41:37 -0700

    Add follow and unfollow functionality.

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,9 +1,6 @@
 #TWEET
 post '/tweets/create' do
   @tweet = Tweet.new(user: current_user, tweet: params[:tweet])
-  @suggested_users = User.all
-  p '*' * 100
-  p @suggested_users
   if @tweet.save
     redirect "/"
   else
