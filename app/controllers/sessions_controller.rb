65c9f3404be598b1d06ba20516c5d320dfe0a81a
Govind Rai <raigovind93@gmail.com>
2017-04-03 21:30:35 -0700

    User signup working with full input and upload validation

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -14,28 +14,6 @@ get '/' do
   end
 end
 
-
-
-
-#FOLLOW AND UNFOLLOW PEOPLE
-post '/:handle/follow' do
-  user_to_follow = User.find_by(handle: params[:handle])
-  Following.create(user: current_user, following_id: user_to_follow.id)
-  Follower.create(user: user_to_follow, follower_id: current_user.id)
-  redirect '/'
-end
-
-delete '/:handle/follow'  do
-  user_to_unfollow = User.find_by(handle: params[:handle])
-  Following.find_by(user: current_user, following_id: user_to_unfollow.id).destroy
-  Follower.find_by(user: user_to_follow, follower_id: current_user.id).destroy
-  redirect '/'
-end
-
-
-
-
-
 post '/login' do
     @user = User.find_by(handle: params[:handle])
   if @user && @user.authenticate(params[:password])
@@ -47,49 +25,21 @@ post '/login' do
 end
 
 post '/signup' do
-  # p params
+  @user = User.new(params[:user])
+  @user.valid?
   @signup_errors = {}
-  @user = User.find_by(handle: params[:user][:handle])
-
-  if @user
-    @user = User.new(params[:user])
-    @signup_errors = "A twitter account with that handle already exists"
+  password_input_validation = password_input_validation(params[:user][:password], params[:confirm_password])
+  profile_image_input_validation = profile_image_input_validation(params)
+  if @signup_errors.length > 0 || profile_image_input_validation || @user.errors.messages.length > 0 || password_input_validation
+    @signup_errors = @user.errors.messages
+    @signup_errors = @signup_errors.merge(password_input_validation) if password_input_validation
+    @signup_errors = @signup_errors.merge(profile_image_input_validation) if profile_image_input_validation
     erb :'index'
   else
-    @user = User.new(params[:user])
-    if params[:user][:password] == "" and params["password-confirm"] == ""
-      @signup_errors[:password] = ["Password fields cannot be blank"]
-    end
-    if params[:user][:password] != params["password-confirm"]
-      @signup_errors[:password] = ["Passwords don't match"]
-    end
-
-    if params[:profile_image]
-    profile_image_hash = params[:profile_image]
-
-    temp_file = profile_image_hash[:tempfile] # <Tempfile:/var/folders/_c/7cmjqt3d051475p3v8xl5dlr0000gn/T/..jpg>
-
-    if @user.valid?
-      if less_than_3mb?(temp_file)
-        @user.save
-        login_user
-        # extra info good to know
-        # filetype = profile_image_hash[:type] # "image/jpeg"
-        # img_headers = profile_image_hash[:head] #the same info as above minus the tempfile
-        filename = profile_image_hash[:filename] # "NBC_SNL_VANESSA_BAYER.jpg"
-        copy_file(temp_file, filename)
-
-        #need a migration to save the file location
-
-        redirect '/'
-        else
-          @signup_errors[:profile_image] = ["Image cannot be greater than 3MB"]
-        end
-      else
-        @signup_errors = @signup_errors.merge(@user.errors.messages)
-        erb :'index'
-      end
-    end
+    @user.save
+    login_user
+    copy_file(params)
+    redirect '/'
   end
 end
 
--- app/helpers/sessions_helpers.rb
+++ app/helpers/sessions_helpers.rb
@@ -10,3 +10,30 @@ def logout_user
   session[:user_id] = nil
 end
 
+def password_input_validation(password, password_confirmation)
+  signup_errors = {}
+
+  if password == "" && password_confirmation == ""
+    signup_errors[:password] = ["Password fields cannot be blank"]
+  end
+
+  if password != password_confirmation
+    signup_errors[:password] = ["Passwords don't match"]
+  end
+
+  if password.length < 6  || password_confirmation.length < 6
+    signup_errors[:password] = ["Passwords must be 6 or more characters"]
+  end
+
+  signup_errors.length > 0 ? signup_errors : false
+end
+
+def profile_image_input_validation(params)
+  signup_errors = {}
+  if params[:profile_image]
+    profile_image_hash = params[:profile_image]
+    temp_file = profile_image_hash[:tempfile] # <Tempfile:/var/folders/_c/7cmjqt3d051475p3v8xl5dlr0000gn/T/..jpg>
+    signup_errors[:profile_image] = ["Image cannot be greater than 3MB"] unless less_than_3mb?(temp_file)
+  end
+  signup_errors.length > 0 ? signup_errors : false
+end
