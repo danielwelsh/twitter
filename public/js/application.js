01c20d263538c2a399c688089394f3796723e2f6
Govind Rai <raigovind93@gmail.com>
2017-04-07 02:01:12 -0700

    Add quick view profile functionality using ajax

--- public/js/application.js
+++ public/js/application.js
@@ -4,8 +4,23 @@ $(document).ready(function() {
   // when we try to bind to them
 
   // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
+  function displayHoverProfile() {
+    var insertLocation = $(this)
+    var URL = '/hover' + $(this).attr('href');
+    console.log('here')
+    var request = $.ajax({
+      method: 'POST',
+      url: URL
+    });
 
+    var done = request.done(function(response) {
+      insertLocation.after(response);
+    });
+  }
+
+  $('.tweets-container').on('mouseenter', '.tweet-user-name', displayHoverProfile);
 
 });
+
 
 
