6a0672fde4152a28b0f8faadcf40b756db0d5919
Daniel Welsh <danielwelsh@borealproductions.com>
2017-04-07 00:53:29 -0700

    Implement retweet ajax

--- public/js/application.js
+++ public/js/application.js
@@ -6,52 +6,52 @@ $(document).ready(function() {
   // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
 
   $('.tweets-container').on('submit', '.like-form', likeListener)
-  $('.tweets-container').on('submit', '.retweet-form', retweetListener)
 
 
   function likeListener (e) {
     e.preventDefault();
-    var type = $(this).attr('method')
+
     var URL = $(this)[0].action
-    // Only passing the tweet id
+    // Passing: tweet_id, _method
     var data = $(this).serialize()
     var insertLocation = $(this).parent()
+
     var request = $.ajax({
-      type: type,
+      type: "POST",
       data: data,
       url: URL
     })
+
     request.done(function(response) {
       // console.log(response)
       insertLocation.replaceWith(response)
     })
+
     request.fail(function () {
       console.log("This did not work out as expected.")
     })
-    }
+  }
+
+
+  $('.tweets-container').on('submit', '.retweet-form', retweetListener)
+
 
     function retweetListener (e) {
       e.preventDefault();
-      var data = $(this).serialize()
-      var type = "POST"
-      console.log(data['_method'])
-      if (data['_method']) {
-        type = "DELETE"
-      }
-      var URL = $(this)[0].action
-
-
 
       // Getting the tweet id
       var data = $(this).serialize()
+      var URL = $(this)[0].action
       var insertLocation = $(this).parent()
+
+
       var request = $.ajax({
-        type: type,
+        type: "POST",
         data: data,
         url: URL
       })
       request.done(function(response) {
-        insertLocation.replaceWith(response)
+        insertLocation.html(response)
       })
       request.fail(function(response) {
         console.log("Something has gone very wrong.")
