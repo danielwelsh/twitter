42fe96cfdcea9115b3e72ba57f21fe6f8788b9af
Daniel Welsh <danielwelsh@borealproductions.com>
2017-04-10 20:45:58 -0700

    Refactor tweet like routes to use partial

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -29,20 +29,11 @@ end
 #LIKE
 post '/tweets/:tweet_id/like' do
   LikedTweet.create(user: current_user, tweet_id: params[:tweet_id])
-  tweet = Tweet.find(params[:tweet_id])
-  likes = tweet.change_likes_count(:+)
+  @tweet = Tweet.find(params[:tweet_id])
+  @likes = @tweet.change_likes_count(:+)
 
   if request.xhr?
-    erb :'__delete_tweet_like'
-    # Passing back the whole delete form
-    %Q(<div class="like-functionality">
-        <form  class="like-form" action="/tweets/#{tweet.id}/like" method="POST">
-          <input type="hidden" name="_method" value="delete">
-          <button class="liked-heart" type="submit">&#x2764;</button>
-        </form>
-        <a id="likes-count" href="">#{likes} </a>
-      </div>
-    )
+    erb :'tweets/_delete_tweet_like'
   else
     redirect '/'
   end
@@ -50,18 +41,11 @@ end
 
 delete '/tweets/:tweet_id/like' do
   LikedTweet.find_by(user: current_user, tweet_id: params[:tweet_id]).destroy
-  tweet = Tweet.find(params[:tweet_id])
-  likes = tweet.change_likes_count(:-)
+  @tweet = Tweet.find(params[:tweet_id])
+  @likes = @tweet.change_likes_count(:-)
 
   if request.xhr?
-    # Passing back the whole delete form
-    %Q(<div class="like-functionality">
-        <form class="like-form" action="/tweets/#{tweet.id}/like" method="POST">
-        <button type="submit">&#x2764;</button>
-        </form>
-        <a id="likes-count" href="">#{likes} </a>
-      </div>
-    )
+    erb :'tweets/_create_tweet_like'
   else
     redirect '/'
   end
